43276a4417ccd478ac3e54caa2d2c2acbc2a808f
DeysonDiaz <ddiazti@unsa.edu.pe>
2021-11-28 11:17:39 -0500

    adding links between templates

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path("registerclient", views.registerclient, name = "registerclient"),
+	path("registerprofessional", views.registerprofessional, name = "registerprofessional"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def registerclient(request):
+    return render(request, 'registerclient.html')
+
+def registerprofessional(request):
+    return render(request, 'registerprofessional.html')
