0c603305769d65ea00b17ba3fb371b740d5edd9d
DarioAncco <aanccor@unsa.edu.pe>
2021-12-18 21:16:15 -0500

    Correcciones

--- offall/settings.py
+++ offall/settings.py
@@ -123,6 +123,9 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL =  '/media/'
+MEDIAFILES_MDIRS = [
+    os.path.join(BASE_DIR, 'media')
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
